6b6ce1818489ec8a6faf255fff530a3101c5453b
Alex Russell <alexrussellpersonal@gmail.com>
2019-05-17 13:19:26 -0500

    Update all

--- src/FlipACoin.java
+++ src/FlipACoin.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class FlipACoin {
+    public static void FlipACoin() {
+            int userEntry = 0;
+        Scanner input = new Scanner(System.in);
+        while (userEntry != 999){
+            System.out.println("Pick heads(1) or tails(2) >>>");
+
+            userEntry = input.nextInt();
+
+
+            if (Math.random() < 0.5) {
+                System.out.println("Heads");
+            } else {
+                System.out.println("Tails");
+            }
+            
+        }
+    }
 }
